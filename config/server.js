890d15407acff431def5bdb394a4f1adaab93d32
unknown <mansadv@gmail.com>
2020-08-05 19:36:26 +0200

    Add all my files

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9bfec7b397ad3cb88bee11e67d528c07'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '20e0967b-6ae0-4ff3-a222-1f20720f4adb'
+};
